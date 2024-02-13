f3eee11e1b79e6460d6cf5b39cc6aac0d51f824e
mdsaidul44 <islam88758@gmail.com>
2024-02-13 12:26:32 +0600

    second card added

--- script/rectangle.area.js
+++ script/rectangle.area.js
@@ -1,3 +1,20 @@
 function calculateRectangleArea(){
-    console.log('rectangle area')
+    // length area
+    const lengthInput = document.getElementById('rectangle-length')
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText) 
+    console.log(length)
+
+    //  width area
+    const widthInput = document.getElementById('rectangle-width')
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText)
+    console.log(width)
+
+    // calculate rectangle area
+    const area = length * width
+    console.log('Area of the rectangle', area)
+    // display rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area')
+    rectangleAreaSpan.innerText= area;
 }
